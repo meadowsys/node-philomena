66a19c31f8e0bad1f77f26eaba84422924eaa582
Alexander Liu <liujalexander84@gmail.com>
2021-01-14 13:48:12 -0800

    fix everything

--- renderdocs.js
+++ renderdocs.js
@@ -1,49 +1,36 @@
+// @ts-check
 "use strict";
 
 if (!process.env.FOLDERNAME) {
-   if (process.env.CI) {
-      console.error("no folder name provided (and this is CI), aborting");
-      process.exit(1);
-   }
+   process.env.CI && (void console.error("no folder name provided (and this is CI), aborting") || process.exit(1));
    process.env.FOLDERNAME = "";
 }
 
-const { readFileSync: readfile } = require("fs");
-const { resolve: resolvepath } = require("path");
-const { parse: json5parse } = require("json5");
-const { CliApplication: typedoccliapp } = require("typedoc");
+const fs = require("fs-extra");
+const { Application, TSConfigReader } = require("typedoc");
 
-const app = new typedoccliapp();
+const app = new Application();
 const input = ["./src/index.ts"];
 const outdir = "docs";
 const out = `${outdir}/${process.env.FOLDERNAME}`;
-const tsconfig = json5parse(readfile(resolvepath(__dirname, "tsconfig.json")));
 const isreleasebuild = process.env.CI ? process.env.GITHUB_REF.includes("/tag") : false;
 
-const opts = {
-   ...tsconfig.compilerOptions,
-   inputFiles: input,
+app.options.addReader(new TSConfigReader);
+
+app.bootstrap({
+   entryPoints: input,
    out: out,
-   mode: "file",
-   // excludeNotExported: true,
    excludePrivate: true,
-   ignoreCompilerErrors: true,
    includeVersion: isreleasebuild,
-   listInvalidSymbolLinks: true,
-   stripInternal: true
-   // ,readme: "none"
-};
+   listInvalidSymbolLinks: true
+});
 
-// shush typedoc lol
-delete opts.sourceMap;
-delete opts.declaration;
-
-app.bootstrap(opts);
+const project = app.convert();
+if (project) app.generateDocs(project, out).catch((...err) => void console.error(...err) || (process.exitCode = 1));
 
 if (isreleasebuild) {
-   const fse = require("fs-extra");
    // also put it in folder "latest"
-   fse.copySync(`${outdir}/${process.env.FOLDERNAME}`, `${outdir}/latest`, {
+   fs.copySync(`${outdir}/${process.env.FOLDERNAME}`, `${outdir}/latest`, {
       errorOnExist: true
    });
 }
